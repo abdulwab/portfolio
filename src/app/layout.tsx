aa140b3b82ff67f8650ea6c32b5db3918ceadc4b
abdulwab <alvilahori@gmail.com>
2025-02-10 23:48:09 +0500

    "feat(layout): update root layout with theme support - Added suppressHydrationWarning - Added font variables - Implemented ThemeProvider - Added ClientWrapper for hydration"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="dark" suppressHydrationWarning>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`} suppressHydrationWarning>
         <ThemeProvider>
           <ClientWrapper>{children}</ClientWrapper>
         </ThemeProvider>
