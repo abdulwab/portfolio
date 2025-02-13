b868dfb938352994e1299b90418d3086650946ea
abdulwab <alvilahori@gmail.com>
2025-02-13 19:31:54 +0500

    Update Next.js config with image optimization and standalone output settings

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      }
+    ],
+    unoptimized: true,
+  },
+  output: 'standalone',
 };
 
-module.exports = nextConfig; 
+module.exports = nextConfig;
