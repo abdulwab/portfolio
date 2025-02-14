a4b7eeca2005a50e046bf347118f28113bdc3480
abdulwab <alvilahori@gmail.com>
2025-02-14 18:55:17 +0500

    tailwind update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -37,9 +37,41 @@ export default {
           '100%': { transform: 'translateY(0)', opacity: '1' },
         },
       },
+      perspective: {
+        '1000': '1000px',
+      },
+      rotate: {
+        'y-180': 'rotateY(180deg)',
+        'y-0': 'rotateY(0deg)',
+      },
+      transformStyle: {
+        '3d': 'preserve-3d',
+      },
+      backfaceVisibility: {
+        'hidden': 'hidden',
+      },
     },
   },
   plugins: [
     require('@tailwindcss/typography'),
+    function ({ addUtilities }) {
+      addUtilities({
+        '.perspective-1000': {
+          perspective: '1000px',
+        },
+        '.rotate-y-180': {
+          transform: 'rotateY(180deg)',
+        },
+        '.rotate-y-0': {
+          transform: 'rotateY(0deg)',
+        },
+        '.transform-style-3d': {
+          transformStyle: 'preserve-3d',
+        },
+        '.backface-hidden': {
+          backfaceVisibility: 'hidden',
+        },
+      });
+    },
   ],
 } satisfies Config;
